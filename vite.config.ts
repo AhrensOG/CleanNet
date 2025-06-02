f62964eee0d4eb2c4e3ac07be372d1a68b497e13
AhrensOG <ahrensgabriel09@gmail.com>
2025-06-02 07:06:48 +0200

    Add vite vonfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
   preview: {
-    allowedHosts: true,
-    host: '0.0.0.0',
+    allowedHosts: ["clean-net.ch", "www.clean-net.ch", "localhost"],
+    host: "0.0.0.0",
     port: 4321,
     strictPort: true,
   },
