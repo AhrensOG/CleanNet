b89bf5c78f36b3b929c4c1ad55f869f0cdd05e9b
AhrensOG <ahrensgabriel09@gmail.com>
2025-06-02 09:31:31 +0200

    Add vite vonfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import alpinejs from "@astrojs/alpinejs";
+import node from "@astrojs/node";
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
   integrations: [alpinejs()],
+  adapter: node({
+    mode: "standalone",
+  }),
 });
