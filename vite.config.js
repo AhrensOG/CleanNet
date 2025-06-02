3f568e9b52b6b1e8917225871e1b7eb5f19c75af
AhrensOG <ahrensgabriel09@gmail.com>
2025-06-02 06:15:22 +0200

    Add vite vonfig

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   preview: {
-    // Permitir tu dominio para la preview
     allowedHosts: ["clean-net.ch", "www.clean-net.ch", "localhost"],
-    port: 4321, // o el puerto que usás
+    port: 4321,
   },
 });
